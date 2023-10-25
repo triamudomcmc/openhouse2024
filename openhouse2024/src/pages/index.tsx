cce5e0a35f7fd44cf7097c19a907c1640053ca5f
pr1gram <139366091+pr1gram@users.noreply.github.com>
2023-10-25 23:48:13 +0700

    Update index.tsx

--- openhouse2024/src/pages/index.tsx
+++ openhouse2024/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import Image from "next/image";
-import LoginPage from "@/pages/login";
 
 export default function Home() {
   return (
-    <LoginPage />
   );
 }
