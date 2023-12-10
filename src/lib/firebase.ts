05f73031c8be14a5f5744d8146eaec4fe8ab8ec8
pr1gram <139366091+pr1gram@users.noreply.github.com>
2023-12-10 17:35:39 +0700

    remove unnecessary lib

--- src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { fireConfig } from "@config/fireConfig"
-import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
-
-const firebaseApp = initializeApp(fireConfig)
-// const analytics = getAnalytics(firebaseApp)
-
-export default firebaseApp
