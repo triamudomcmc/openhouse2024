64e94118240c49840f2ea69db87f7d09bc9fc509
pr1gram <139366091+pr1gram@users.noreply.github.com>
2024-01-06 15:52:58 +0700

    patch search

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -144,6 +144,9 @@ export default function Map() {
     if (searchQuery !== "") {
       handleNameSearch();
     }
+    else{
+      setMapId(mapData.map((item) => item.id))
+    }
   }, [searchQuery]);
 
   return (
